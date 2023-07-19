01e8a5970be697b90fd87e3bcd7789a177f5c273
thasipablo <thasipablo@gmail.com>
2023-07-19 13:28:48 -0400

    add checkboxes to the todo items

--- src/index.js
+++ src/index.js
@@ -20,13 +20,20 @@ todos.forEach((todo)=>{
   todoElement.classList.add('todo-item');
 
   const description = document.createElement('div');
-  description.innerHTML = todo.description;
+  description.classList.add('todo-item-description');
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  const descriptionText = document.createElement('div');
+  descriptionText.innerHTML = todo.description;
   const icon = document.createElement('div');
   icon.innerHTML = `
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
   </svg>
   `
+
+  description.appendChild(checkBox);
+  description.appendChild(descriptionText);
   todoElement.appendChild(description);
   todoElement.appendChild(icon);
   todoElement.classList.add('component');
